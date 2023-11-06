a745245e4878a0732cdd7b0251cbace2a8113ce6
Fairuz <akbarazariafairuz@gmail.com>
2023-11-06 11:04:50 -0600

    Scheduling GA v0.1

--- schedule_data.py
+++ schedule_data.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import pandas as pd
+import random
 
 def get_activities_data():
     list_data = pd.read_csv(
@@ -8,8 +10,8 @@ def get_activities_data():
     
     data = {}
     for i, row in list_data.iterrows():
-        prefered_facilitators = row['prefered_facilitators'].split(", ")
-        other_facilitators = row['other_facilitators'].split(", ")
+        prefered_facilitators = tuple(row['prefered_facilitators'].split(", "))
+        other_facilitators = tuple(row['other_facilitators'].split(", "))
 
         data[row['activity']] = {'expected_enrollment': row['expected_enrollment'],
                                  'prefered_facilitators': prefered_facilitators,
@@ -18,20 +20,48 @@ def get_activities_data():
 
 def get_all_facilitators():
     list_data = pd.read_csv('schedule_data/facilitators.csv', names=['facilitators'])
-    data = {row['facilitators'] for i, row in list_data.iterrows()}
+    data = tuple(row['facilitators'] for i, row in list_data.iterrows())
     return data
 
 def get_available_times():
     list_data = pd.read_csv('schedule_data/times.csv', names=['times'])
-    data = {row['times'] for i, row in list_data.iterrows()}
+    data = tuple(convert_time_format(row['times']) for i, row in list_data.iterrows())
     return data
 
 def get_all_rooms():
     list_data = pd.read_csv('schedule_data/rooms.csv', names=['rooms','capacity'])
-    data = {}
-    for i, row in list_data.iterrows():
-        data[row['rooms']] = row['capacity']
+    data = tuple(tuple([row['rooms'],row['capacity']]) for i, row in list_data.iterrows())
     return data
 
 def get_activity_names(activities):
-    return {i for i in activities.keys()}
+    return tuple(i for i in activities.keys())
+
+def initialize_population(activities_name, rooms, times, facilitators, population_number):
+    random_solutions = []
+    for i in range(population_number):
+        solution = list([
+            i, 
+            random.randint(0, rooms-1), 
+            random.randint(0, times-1), 
+            random.randint(0, facilitators-1)] for i in range(activities_name))
+        random_solutions.append(solution)
+    
+    return tuple(random_solutions)
+
+def get_function_inputs(activities_name, rooms, times, facilitators):
+    # function_input = list([activity, random.choice(rooms), random.choice(times), random.choice(facilitators)] for activity in activities_name)   
+    function_input = list([
+        i, 
+        random.randint(0, rooms-1), 
+        random.randint(0, times-1), 
+        random.randint(0, facilitators-1)] for i in range(activities_name))
+
+    return function_input
+
+def get_gene_space(activities_name, rooms, times, facilitators):
+    return [list(range(0, activities_name)), list(range(0, rooms)), list(range(0, times)), list(range(0, facilitators))]
+    
+def convert_time_format(time):
+    # convert time to 24 hour format
+    new_hour_format = datetime.strptime(time, '%I %p')
+    return new_hour_format.strftime('%H')
